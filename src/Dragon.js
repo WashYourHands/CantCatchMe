11af15bfa6494ef8b879563fa718c9f0c4d0428d
Artyom Neustroev <neustroev.ai@gmail.com>
2015-10-19 22:53:39 +0500

    Actual code is here now

--- src/Dragon.js
+++ src/Dragon.js
@@ -1,3 +1,18 @@
-/**
- * Created by artemneustroev on 19/10/15.
- */
+var babylon = require('babylonjs');
+
+const TEXTURE_PATH = 'textures/dragon.png';
+export default class Dragon {
+  constructor(options) {
+    // First animated player
+    this.sprite = new babylon.Sprite("player", new babylon.SpriteManager('dragonManager', TEXTURE_PATH, 2, 128, options.scene));
+    this.sprite.playAnimation(12, 16, true, 100);
+    this.sprite.position.z = -19;
+    this.sprite.position.y = -8;
+    this.sprite.size = 1;
+    this.sprite.isPickable = true;
+
+    options.scene.registerBeforeRender(() => {
+      this.sprite.position.z += 0.1;
+    });
+  }
+}
--- src/Ground.js
+++ src/Ground.js
@@ -1,3 +1,15 @@
-/**
- * Created by artemneustroev on 19/10/15.
- */
+var babylon = require('babylonjs');
+
+export default class Ground {
+  constructor(options) {
+    this.mesh = babylon.Mesh.CreatePlane('ground', 20.0, options.scene);
+    this.mesh.material = new babylon.StandardMaterial('groundMat', options.scene);
+    this.mesh.material.diffuseColor = new babylon.Color3(1, 1, 1);
+    this.mesh.material.backFaceCulling = false;
+    this.mesh.position = new babylon.Vector3(5, -10, -15);
+    this.mesh.rotation = new babylon.Vector3(Math.PI / 2, 0, 0);
+
+    //finally, say which mesh will be collisionable
+    this.mesh.checkCollisions = true;
+  }
+}
