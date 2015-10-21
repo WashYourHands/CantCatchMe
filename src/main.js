29b28038da0c679fbc1a1de4c8653388c201c7b0
artyom <artyom.void@ya.ru>
2015-10-21 15:31:18 +0300

    Status bars

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Game from "./Game.js";
 
 window.addEventListener('DOMContentLoaded', function(){
+  var canvas = document.getElementById('renderCanvas');
   var game = new Game({
-    canvas: document.getElementById('renderCanvas')
+    canvas: canvas
   });
 
   game.start();
+  canvas.height = canvas.width*9/16;
 });
